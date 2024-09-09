8b5270295da97fe6007aad0f3c73f349f64af13f
DeepeshSuryavanshi <suryavanshideepesh04@gmail.com>
2024-09-09 11:11:53 +0530

    array reduce modified

--- Prectis Javascript Programming/Array methods/reduce.js
+++ Prectis Javascript Programming/Array methods/reduce.js
@@ -1,4 +1,4 @@
-// Executes a reducer function on each element of the array, resulting in a single output value.let numbers = [1, 2, 3, 4];
+// Executes a reducer function on each element of the array, resulting in a single output value.
 let numbers = [1, 2, 3, 4];
 let sum = numbers.reduce(function(total, num) {
     return total + num;
