3fe52515ef86b789f979fb4a6be5e5ea935cc21a
Deepesh suryawanshi <deepeshsuryawanshi04@gmail.com>
2025-01-14 13:22:53 +0530

    Add a closers in the folder

---
+++ Closers Scoping/Closers01.js
@@ -0,0 +1,17 @@
+function car(){
+    let car = "BMW";
+    function home(){
+        let home = "Farm house";
+        function me(){
+            let me = "Deepesh";
+            return `${me} have a ${home} and ${car} car`
+        }
+        return me;
+    }
+    return home;
+}
+
+let home = car();
+let me = home()
+let intro = me();
+console.log(intro);
