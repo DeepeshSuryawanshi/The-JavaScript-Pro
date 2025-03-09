f6b0010190ec3ece9912d4dd571ecce227951514
Deepesh suryawanshi <deepeshsuryawanshi04@gmail.com>
2025-03-09 15:39:42 +0530

    update the lru system in code and improving it.

--- AdvanceConcept/Interview Question/LRU.js
+++ AdvanceConcept/Interview Question/LRU.js
@@ -1,46 +1,91 @@
-/*
-interface node{
-    next:value | null;
-    prev:value | null;
-    value : value
-}
-*/
-
-class LRUCashe{
-    constructor(capacity){
+class LRUCashe {
+    constructor(capacity) {
         this.capacity = Number(capacity);
-        this.map = new Map() // <key> : <node:user>:address
+        this.length = 0;
+        this.map = new Map() // <key> : <node:address>
         // pointers in the hash map
         this.head = null;
         this.tail = null;
     }
-    
-    #removeNode(node){
+
+    #removeNode(node) {
+        if(!node) return;   
+        // console
+        console.log(`removing the node ${node.key}: ${node.value}`);
         if (node.prev) {
-            node.prev == node.next
+            console.log(`setting previous the node ${node.key}: ${node.value}`);
+            node.prev = node.next
         }
         if (node.next) {
+            console.log(`setting Next the node ${node.key}: ${node.value}`);
             node.next.prev = node.prev
         }
-        if(node === this.head) this.head = node.next;
-        if(node === this.tail) this.tail = node.prev;
-    }
-
-    get(key){
-
-    }
-
-    put (key, value){
-         // if key aready is there in cashed
-        if(this.map.has(key)){
-            this.#removeNode(this.map.get(key))
+        if (node === this.head) {
+            console.log(`Updating the head ${node.key}: ${node.value}`);
+            this.head = node.next;
         }
+        if (node === this.tail){ 
+            console.log(`updating the tail ${node.key}: ${node.value}`);
+            this.tail = node.prev;
+        }
+    }
+
+    get(key) {
+        return this.map;
+    }
+
+    // put method in the LRU
+    put(key, value) {
+
         const node = {
-            next : this.head,
+            next:this.head,
             prev:null,
+            key,
             value
         }
-        this.map.set(key,node);
+
+        // check capacity 
+        if (this.length === this.capacity) {
+            // chack the key is exist if not so remove the last node 
+            if (!this.map.has(key)) {
+                this.#removeNode(this.tail)
+            }
+        }
+
+        // if key aready is there in cashed
+        if (this.map.has(key)) {
+            this.#removeNode(this.map.get(key))
+        }
+
+        // map /link list 
+        this.map.set(key, node);
+
+        // pointing in the head 
         this.head = node;
+
+        // checking the tail 
+        if (this.tail === null) this.tail = this.head
+
+        // increasing the length of list 
+        this.length += 1;
+
+    }
+
+    // for degugginr
+    debug() {
+        let current = this.head;
+        const arr = [];
+        while (current) {
+            arr.push(current)
+            current = current.next;
+        }
+        // return arr.reduce((arr, curr) => arr.concat(`-->[{${curr.key} : ${curr.value}}]`), "")
+        return arr;
     }
 }
+const cache = new LRUCashe(3)
+cache.put(1, 10)
+cache.put(2, 20)
+cache.put(3, 30)
+cache.put(4, 40)
+console.log(cache.debug())
---
+++ AdvanceConcept/Interview Question/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(cache.get(4))
